e5a04e5b41a5e6a95258c07fe99190dd0ff4f2db
Haoran Qi <DevinQi@outlook.com>
2022-09-26 18:11:54 -0400

    unity configs

---
+++ configs/recognition/unity/swin_base_patch244_window877_22k_on_kinetics.py
@@ -0,0 +1,128 @@
+_base_ = [
+    '../../_base_/models/swin/swin_base.py', '../../_base_/default_runtime.py'
+]
+model=dict(backbone=dict(patch_size=(2,4,4), drop_path_rate=0.2),cls_head=dict(num_classes=7), test_cfg=dict(max_testing_views=2))
+
+# dataset settings
+dataset_type = 'VideoDataset'
+data_root = 'data/unityTrain/videos'
+data_root_val = 'data/unityTrain/videos'
+ann_file_train = 'data/unityTrain/freshkinetics/ftrain.txt'
+ann_file_val = 'data/unityTrain/freshkinetics/fval.txt'
+ann_file_test = 'data/unityTrain/freshkinetics/fval.txt'
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_bgr=False)
+train_pipeline = [
+    dict(type='DecordInit'),
+    dict(type='SampleFrames', clip_len=32, frame_interval=2, num_clips=1),
+    dict(type='DecordDecode'),
+    dict(type='Resize', scale=(-1, 256)),
+    dict(type='RandomResizedCrop'),
+    dict(type='Resize', scale=(224, 224), keep_ratio=False),
+    dict(type='Flip', flip_ratio=0.5),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='FormatShape', input_format='NCTHW'),
+    dict(type='Collect', keys=['imgs', 'label'], meta_keys=[]),
+    dict(type='ToTensor', keys=['imgs', 'label'])
+]
+val_pipeline = [
+    dict(type='DecordInit'),
+    dict(
+        type='SampleFrames',
+        clip_len=32,
+        frame_interval=2,
+        num_clips=1,
+        test_mode=True),
+    dict(type='DecordDecode'),
+    dict(type='Resize', scale=(-1, 256)),
+    dict(type='CenterCrop', crop_size=224),
+    dict(type='Flip', flip_ratio=0),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='FormatShape', input_format='NCTHW'),
+    dict(type='Collect', keys=['imgs', 'label'], meta_keys=[]),
+    dict(type='ToTensor', keys=['imgs'])
+]
+test_pipeline = [
+    dict(type='DecordInit'),
+    dict(
+        type='SampleFrames',
+        clip_len=32,
+        frame_interval=2,
+        num_clips=4,
+        test_mode=True),
+    dict(type='DecordDecode'),
+    dict(type='Resize', scale=(-1, 224)),
+    dict(type='ThreeCrop', crop_size=224),
+    dict(type='Flip', flip_ratio=0),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='FormatShape', input_format='NCTHW'),
+    dict(type='Collect', keys=['imgs', 'label'], meta_keys=[]),
+    dict(type='ToTensor', keys=['imgs'])
+]
+data = dict(
+    videos_per_gpu=8,
+    workers_per_gpu=4,
+    val_dataloader=dict(
+        videos_per_gpu=1,
+        workers_per_gpu=1
+    ),
+    test_dataloader=dict(
+        videos_per_gpu=1,
+        workers_per_gpu=1
+    ),
+    train=dict(
+        type=dataset_type,
+        ann_file=ann_file_train,
+        data_prefix=data_root,
+        pipeline=train_pipeline),
+    val=dict(
+        type=dataset_type,
+        ann_file=ann_file_val,
+        data_prefix=data_root_val,
+        pipeline=val_pipeline),
+    test=dict(
+        type=dataset_type,
+        ann_file=ann_file_test,
+        data_prefix=data_root_val,
+        pipeline=test_pipeline))
+evaluation = dict(
+    interval=5, metrics=['top_k_accuracy', 'mean_class_accuracy'])
+
+# optimizer
+optimizer = dict(type='AdamW', lr=3e-4, betas=(0.9, 0.999), weight_decay=0.05,
+                 paramwise_cfg=dict(custom_keys={'absolute_pos_embed': dict(decay_mult=0.),
+                                                 'relative_position_bias_table': dict(decay_mult=0.),
+                                                 'norm': dict(decay_mult=0.),
+                                                 'backbone': dict(lr_mult=0.1)}))
+# learning policy
+lr_config = dict(
+    policy='CosineAnnealing',
+    min_lr=0,
+    warmup='linear',
+    warmup_by_epoch=True,
+    warmup_iters=2.5
+)
+total_epochs = 30
+
+# runtime settings
+checkpoint_config = dict(interval=1)
+work_dir = './work_dirs/unity/swin_base_patch244_window877_22k_on_kinetics.py'
+find_unused_parameters = False
+
+log_config = dict(  # 注册日志钩子的设置
+    interval=30,  # 打印日志间隔
+    hooks=[  # 训练期间执行的钩子
+        dict(type='TextLoggerHook'),  # 记录训练过程信息的日志
+        dict(type='TensorboardLoggerHook'),  # 同时支持 Tensorboard 日志
+    ])
+
+# do not use mmdet version fp16
+fp16 = None
+optimizer_config = dict(
+    type="DistOptimizerHook",
+    update_interval=8,
+    grad_clip=None,
+    coalesce=True,
+    bucket_size_mb=-1,
+    use_fp16=True,
+)
