a8e8733f5c462dae234e83cb7153b01d38545de3
Haoran Qi <DevinQi@outlook.com>
2023-05-23 19:42:45 +0000

    swin configs

---
+++ configs/skeleton/poseformer/gt/swin_base_patch244_window877_gymgt_22k_both_1to3_channel.py
@@ -0,0 +1,139 @@
+_base_ = [
+    '../../../_base_/models/swin/swin_base.py', '../../../_base_/default_runtime.py'
+]
+model=dict(backbone=dict(patch_size=(2,4,4), in_chans=3, drop_path_rate=0.3), cls_head=dict(num_classes=99), test_cfg=dict(max_testing_views=4))
+
+# dataset settings
+dataset_type = 'PoseDataset'
+ann_file_train = 'data/gymgt/gt-train.pkl'
+ann_file_val = 'data/gymgt/gt-val.pkl'
+left_kp = [1, 3, 5, 7, 9, 11, 13, 15]
+right_kp = [2, 4, 6, 8, 10, 12, 14, 16]
+
+train_pipeline = [
+    dict(type='UniformSampleFrames', clip_len=48),
+    dict(type='PoseDecode'),
+    dict(type='PoseCompact', hw_ratio=1., allow_imgpad=True),
+    dict(type='Resize', scale=(-1, 256)),
+    dict(type='RandomResizedCrop', area_range=(0.56, 1.0)),
+    dict(type='Resize', scale=(128, 128), keep_ratio=False),
+    dict(type='Flip', flip_ratio=0.5, left_kp=left_kp, right_kp=right_kp),
+    dict(
+        type='GeneratePoseTarget',
+        sigma=0.6,
+        use_score=True,
+        shrink_mode=3,
+        with_kp=True,
+        with_limb=True),
+    dict(type='FormatShape', input_format='NCTHW'),
+    dict(type='Collect', keys=['imgs', 'label'], meta_keys=[]),
+    dict(type='ToTensor', keys=['imgs', 'label'])
+]
+val_pipeline = [
+    dict(type='UniformSampleFrames', clip_len=48),
+    dict(type='PoseDecode'),
+    dict(type='PoseCompact', hw_ratio=1., allow_imgpad=True),
+    dict(type='Resize', scale=(-1, 256)),
+    dict(type='RandomResizedCrop', area_range=(0.56, 1.0)),
+    dict(type='Resize', scale=(128, 128), keep_ratio=False),
+    dict(type='Flip', flip_ratio=0.5, left_kp=left_kp, right_kp=right_kp),
+    dict(
+        type='GeneratePoseTarget',
+        sigma=0.6,
+        use_score=True,
+        shrink_mode=3,
+        with_kp=True,
+        with_limb=True),
+    dict(type='FormatShape', input_format='NCTHW'),
+    dict(type='Collect', keys=['imgs', 'label'], meta_keys=[]),
+    dict(type='ToTensor', keys=['imgs', 'label'])
+]
+test_pipeline = [
+    dict(type='UniformSampleFrames', clip_len=48),
+    dict(type='PoseDecode'),
+    dict(type='PoseCompact', hw_ratio=1., allow_imgpad=True),
+    dict(type='Resize', scale=(-1, 256)),
+    dict(type='RandomResizedCrop', area_range=(0.56, 1.0)),
+    dict(type='Resize', scale=(128, 128), keep_ratio=False),
+    dict(type='Flip', flip_ratio=0.5, left_kp=left_kp, right_kp=right_kp),
+    dict(
+        type='GeneratePoseTarget',
+        sigma=0.6,
+        use_score=True,
+        shrink_mode=3,
+        with_kp=True,
+        with_limb=True),
+    dict(type='FormatShape', input_format='NCTHW'),
+    dict(type='Collect', keys=['imgs', 'label'], meta_keys=[]),
+    dict(type='ToTensor', keys=['imgs', 'label'])
+]
+data = dict(
+    videos_per_gpu=8,
+    workers_per_gpu=4,
+    val_dataloader=dict(
+        videos_per_gpu=1,
+        workers_per_gpu=1
+    ),
+    test_dataloader=dict(
+        videos_per_gpu=1,
+        workers_per_gpu=1
+    ),
+    train=dict(
+        type=dataset_type,
+        ann_file=ann_file_train,
+        data_prefix="",
+        pipeline=train_pipeline),
+    val=dict(
+        type=dataset_type,
+        ann_file=ann_file_val,
+        data_prefix="",
+        pipeline=val_pipeline),
+    test=dict(
+        type=dataset_type,
+        ann_file=ann_file_val,
+        data_prefix="",
+        pipeline=test_pipeline))
+evaluation = dict(
+    interval=5, metrics=['top_k_accuracy', 'mean_class_accuracy'])
+
+# optimizer
+optimizer = dict(type='AdamW', lr=3e-4, betas=(0.9, 0.999), weight_decay=0.002,
+                 paramwise_cfg=dict(custom_keys={'absolute_pos_embed': dict(decay_mult=0.),
+                                                 'relative_position_bias_table': dict(decay_mult=0.),
+                                                 'norm': dict(decay_mult=0.),
+                                                 'backbone': dict(lr_mult=0.1)}))
+# learning policy
+lr_config = dict(
+    policy='CosineAnnealing',
+    min_lr=1e-10,
+    warmup='linear',
+    warmup_by_epoch=True,
+    warmup_iters=2
+)
+
+log_config = dict(  # 注册日志钩子的设置
+    interval=40,  # 打印日志间隔
+    hooks=[  # 训练期间执行的钩子
+        dict(type='TextLoggerHook'),  # 记录训练过程信息的日志
+        dict(type='TensorboardLoggerHook'),  # 同时支持 Tensorboard 日志
+    ])
+total_epochs = 100
+
+# load_from =  "/home/haoran/Video-Swin-Transformer/work_dirs/poseformer/continue_4_three_swin_base_patch244_window877_gym_22k_both_three_channel/epoch_55.pth"
+
+# runtime settings
+checkpoint_config = dict(interval=1)
+work_dir = './work_dirs/GT_Poseformer/three_swin_base_patch244_window877_gymgt_22k_both_1to3_channel'
+find_unused_parameters = False
+
+optimizer_config = dict(grad_clip=None)
+# do not use mmdet version fp16
+fp16 = None
+# optimizer_config = dict(
+#     type="DistOptimizerHook",
+#     update_interval=8,
+#     grad_clip=None,
+#     coalesce=True,
+#     bucket_size_mb=-1,
+#     use_fp16=True,
+# )
